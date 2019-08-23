f44662e8cebcf9a691711643195408f19d8afdcd
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-08-23 09:23:36 -0500

    actualizado

--- MayorDeTresNumeros/main.cpp
+++ MayorDeTresNumeros/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,b,c;
+
+    cout << "Ingrese primer numero" << endl;
+    cin >> a;
+    cout << "Ingrese segundo numero" << endl;
+    cin >> b;
+    cout << "Ingrese tercer numero" << endl;
+    cin >> c;
+
+    if (a>=b){
+        if (a>=c)
+            cout << a << " es el mayor numero" << endl;
+        else
+            cout << c << " es el mayor numero" << endl;
+    }
+    else{
+        if (b>=c)
+            cout << b << " es el mayor numero" << endl;
+        else
+            cout << c << " es el mayor numero" << endl;
+    }
     return 0;
 }
